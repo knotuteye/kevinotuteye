31cb96359c8260c56e12a35ce98cf03e7c4ff624
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-21 23:44:17 +0000

    finished profile pic styling

--- sections/Profile.tsx
+++ sections/Profile.tsx
@@ -1,12 +1,19 @@
 import Image from 'next/image'
+
 export default function Profile({}) {
   return (
-    <div id="profile" className="flex h-96 justify-center items-center">
-      <div className="flex w-64 h-72 rounded-full">
+    <div id="profile" className="flex justify-center items-center" style={{}}>
+      <div
+        className="flex my-20 rounded-full"
+        style={{ height: '20rem', width: '19rem' }}
+      >
         <Image
           src={require('/public/img/profile.jpg')}
           className="rounded-full"
         ></Image>
+      </div>
+      <div>
+        
       </div>
     </div>
   )
