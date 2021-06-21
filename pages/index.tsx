0ce50a306c7badbc729d8f40073406e72d57efdb
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-21 23:08:52 +0000

    added profile pic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Header from '../sections/Header'
 import Navbar from '../sections/Navbar'
+import Profile from '../sections/Profile'
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
       <Navbar></Navbar>
+      <Profile></Profile>
     </div>
   )
 }
--- sections/Profile.tsx
+++ sections/Profile.tsx
@@ -1,5 +1,13 @@
-export default function Profile({ }) {
-    return <div id="profile">
-        
+import Image from 'next/image'
+export default function Profile({}) {
+  return (
+    <div id="profile" className="flex h-96 justify-center items-center">
+      <div className="flex w-64 h-72 rounded-full">
+        <Image
+          src={require('/public/img/profile.jpg')}
+          className="rounded-full"
+        ></Image>
+      </div>
     </div>
+  )
 }
