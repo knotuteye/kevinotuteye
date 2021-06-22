62ca8625b0bad9d32dcd487fb9633e385abbfbc8
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-22 16:09:43 +0000

    added contact form

---
+++ pages/contact.tsx
@@ -0,0 +1,13 @@
+import ContactForm from '../sections/ContactForm'
+import Header from '../sections/Header'
+import Navbar from '../sections/Navbar'
+
+export default function Contact() {
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <ContactForm></ContactForm>
+    </div>
+  )
+}
---
+++ sections/ContactForm.tsx
@@ -0,0 +1,5 @@
+export default function ContactForm({ }) {
+    return <div>
+        
+    </div>
+}
