6068d809ad387f1f4b2e4a79eca4a0c7f3102b9b
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-22 17:07:27 +0000

    wired up backdrop for contact modal

--- sections/ContactForm.tsx
+++ sections/ContactForm.tsx
@@ -1,5 +1,48 @@
-export default function ContactForm({ }) {
-    return <div>
-        
+import { Dispatch, SetStateAction } from 'react'
+import Button from '../components/Button'
+type Props = {
+  show: boolean
+  setShow: Dispatch<SetStateAction<boolean>>
+}
+export default function ContactForm({ show, setShow }: Props) {
+  function dismissHandler(e: HTMLDivElement) {
+    if (e.id === 'backdrop') setShow(false)
+  }
+  return show ? (
+    <div
+      id="backdrop"
+      className="flex h-screen w-screen justify-center items-center fixed top-0"
+      onClick={(e) => dismissHandler(e.target as HTMLDivElement)}
+      style={{ backgroundColor: '#000000aa' }}
+    >
+      <form
+        action=""
+        className="flex flex-col bg-white max-w-md my-20 rounded-xl px-10 py-10 gap-y-5 font-body"
+        style={{ width: '100%' }}
+      >
+        <input
+          className="border-2 border-zomp rounded-md text-sm px-3 py-2"
+          type="text"
+          name="sender"
+          placeholder="Name"
+        />
+        <input
+          className="border-2 border-queenBlue rounded-md text-sm px-3 py-2"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <textarea
+          className="border-2 border-maizeCrayola rounded-md text-sm px-3 py-2"
+          name="message"
+          cols={30}
+          rows={10}
+          placeholder="Message"
+        ></textarea>
+        <Button text="Send"></Button>
+      </form>
     </div>
+  ) : (
+    <></>
+  )
 }
--- sections/Navbar.tsx
+++ sections/Navbar.tsx
@@ -1,8 +1,11 @@
 import { useRouter } from 'next/dist/client/router'
+import { useState } from 'react'
 import Button from '../components/Button'
 import Masthead from '../components/Masthead'
+import ContactForm from './ContactForm'
 
 export default function Navbar() {
+  const [showContactForm, setShowContactForm] = useState(false)
   const router = useRouter()
   const navButtons = [
     {
@@ -15,33 +18,37 @@ export default function Navbar() {
       text: 'Work',
       onClick: () => {
         router.push('work')
-
       },
     },
     {
       text: 'Say Hi',
       onClick: () => {
-        router.push('contact')
-
+        setShowContactForm(true)
       },
       alt: true,
     },
   ]
   return (
-    <div className="flex items-center py-5 " style={{ padding: '2rem' }}>
-      <div>
-        <Masthead></Masthead>
+    <>
+      <div className="flex items-center py-5 " style={{ padding: '2rem' }}>
+        <div>
+          <Masthead></Masthead>
+        </div>
+        <div className="flex flex-1 gap-x-5 justify-end font-body">
+          {navButtons.map((btn, i) => (
+            <Button
+              text={btn.text}
+              alt={btn.alt}
+              onClick={btn.onClick}
+              key={btn.text}
+            ></Button>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-1 gap-x-5 justify-end font-body">
-        {navButtons.map((btn, i) => (
-          <Button
-            text={btn.text}
-            alt={btn.alt}
-            onClick={btn.onClick}
-            key={btn.text}
-          ></Button>
-        ))}
-      </div>
-    </div>
+      <ContactForm
+        show={showContactForm}
+        setShow={setShowContactForm}
+      ></ContactForm>
+    </>
   )
 }
