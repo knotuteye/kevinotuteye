337e78dbf7bf3b84c8fbe95ed6831051ed3c8864
Kevin Otuteye <kevinotuteye@gmail.com>
2021-06-22 01:38:34 +0000

    changed avatar to flaticon

--- sections/Profile.tsx
+++ sections/Profile.tsx
@@ -1,18 +1,23 @@
 import Image from 'next/image'
 
 export default function Profile({}) {
-  const profilePic = require('/public/img/profile.jpg')
   return (
     <div id="profile" className="flex flex-col justify-center items-center">
       <div
         className="flex my-20 rounded-full border-4 border-pistachio"
-        style={{ height: '20rem', width: '20rem' }}
-      >
-        <Image src={profilePic} className="rounded-full"></Image>
-      </div>
+        style={{
+          height: '20rem',
+          width: '20rem',
+          backgroundImage: 'url(/img/profile.png)',
+          backgroundPosition: 'center bottom',
+          backgroundSize: '85%',
+          backgroundRepeat: 'no-repeat',
+        }}
+      ></div>
       <div className="flex flex-col gap-y-3 text-2xl max-w-xl font-body text-center text-queenBlue">
         <span>
-          Hi there, I'm <span className=" text-maizeCrayola font-bold">Kevin Otuteye.</span>
+          Hi there, I'm{' '}
+          <span className=" text-maizeCrayola font-bold">Kevin Otuteye.</span>
         </span>
         <span>
           I create beautiful digital experiences from web to desktop to mobile.
